8838e5be0c3b7f8d31d81fe35e70e8b5acd00e25
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2025-02-18 14:15:01 +0900

    feat: add /audio router

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import path from "path";
 import wordRoutes from "@/route/wordRoutes";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.use("/audio", express.static(path.join(__dirname, "../audio")));
 
 app.use("/", wordRoutes);
 
